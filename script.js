2566554a1461d0425303f261a9afef9dacda2316
James Farrell <jamesfarrellmusic@gmail.com>
2019-01-19 22:27:45 -0600

    code works. win. lose. draw.

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let turn = 1
-let box = document.querySelectorAll(".button__box")
+let winner = false
+let game = 1
+let xArray = []
+let oArray = []
+
+
+let winningCombos = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9]
+]
+
+
+const winCheck = (player, playerArray) => winningCombos.forEach((combo) => {
+    if (playerArray.includes(combo[0]) && playerArray.includes(combo[1]) && playerArray.includes(combo[2])) {
+        winner = true
+        alert(`${player} Wins!`)
+    }
+})
+
+if (game === 10 && winner === false) {
+    alert("It's a tie")
+}
 
 let buttonClicked = (id) => {
     document.getElementById(id).addEventListener("click", function () {
@@ -7,15 +34,26 @@ let buttonClicked = (id) => {
             button = document.getElementById(id)
             button.innerHTML = "X"
             turn++
-            button.disabled=true
+            game++
+            button.disabled = true
+            xArray.push(parseInt(id))
+            winCheck("X", xArray)
+            if (game === 10 && winner === false) {
+                alert("It's a tie")
+            }
         } else {
             if (turn === 2) {
                 button = document.getElementById(id)
                 button.innerHTML = "O"
                 button.className += " blue"
-                console.log(button)
                 turn--
-                button.disabled=true
+                game++
+                button.disabled = true
+                oArray.push(parseInt(id))
+                winCheck("O", oArray)
+                if (game === 10 && winner === false) {
+                    alert("It's a tie")
+                }
             }
         }
     });
